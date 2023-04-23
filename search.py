5f567e2e9a0ee3ab432e268c632ad7a2aa2083f4
DrHannibalLecter <ardacanozener@gmail.com>
2023-04-23 12:28:37 +0100

    Done

--- search.py
+++ search.py
@@ -1,24 +1,27 @@
 import sys
 import time
 
-"""Kanka Python'da fonksiyon isimleri küçük harf oluyor.
-Böyle bir hatayı senin gibi nizami yazan adama yakıştıramadım."""
 
 start_time = time.time()
 
 comparisons = 0
 
+# Bad symbol table as dictionary
 btable = {}
 
+# Good suffix table as list
 gtable = []
 
+# Pattern's length-1. It will be assigned later
 length = 0
 
 with open('test.html', 'r') as file:
     lines = file.readlines()
     
 
-def conbadtable(word):
+# Construction of bad symbol table
+def conbadtable():
+    global word
     global btable
     global length
     length = len(word) - 1
@@ -28,13 +31,19 @@ def conbadtable(word):
         i += 1
 
 
-def congoodtable(word):
+#Construction of good suffix table
+def congoodtable():
+    global word
     global gtable
     global length
+    # Since we construct the bad symbol table, we know previous occurrence of pattern's last letter
     try:
         i = length - btable[word[length]]
+    # If there is a KeyError, pattern's last letter is not in pattern
+    # So all values must be length of pattern
     except KeyError:
         return
+    # lgtable is length of good suffix table + 1
     lgtable = 1
     le = lgtable
     while i >= 0:
@@ -53,11 +62,14 @@ def congoodtable(word):
             le = lgtable
     gtable.append(length - le - 1)
     lgtable -= 1
+    # If index (i) of pattern becomes 0 and if length of good suffix table is less than pattern's length,
+    # Add last value of good suffix table to rest of table's empty values.
     for i in range(length-lgtable-1):
         gtable.append(gtable[lgtable])
 
 
-def horspoolsearch(word):
+def horspoolsearch():
+    global word
     global length
     global comparisons
     global line
@@ -66,25 +78,29 @@ def horspoolsearch(word):
     a = length
     while i < size:
         comparisons += 1
+        # Character match
         if line[i] == word[a]:
             if a:
                 i -= 1
                 a -= 1
-            #There is a match
+            #Pattern match
             else:
                 a = length
                 line = line[:i] + '<mark>' + word + '</mark>' + line[i + length + 1:]
                 i += (length + 14)
         else:
+            c = length - a
+            # If pattern's last letter's comparison is in bad symbol table,
+            # Increases index by it. Otherwise, increases it by pattern's length
             try:
-                c = length - a
                 i += (btable[line[i + c]] + c)
             except KeyError:
                 i += (length + 1)
             a = length
 
 
-def boyermoore(word):
+def boyermoore():
+    global word
     global length
     global comparisons
     global line
@@ -93,11 +109,12 @@ def boyermoore(word):
     a = length
     while i < size:
         comparisons += 1
+        #Character match
         if line[i] == word[a]:
             if a:
                 i -= 1
                 a -= 1
-            #There is a match
+            #Pattern match
             else:
                 a = length
                 line = line[:i] + '<mark>' + word + '</mark>' + line[i + length + 1:]
@@ -105,6 +122,8 @@ def boyermoore(word):
         else:
             c = length - a
             if c:
+                # If last comparison is in bad symbol table and good suffix table, applies formula
+                # Otherwise, increases it by pattern's length
                 try:
                     temp = max(btable[line[i]] - c, 1)
                     temp2 = gtable[c - 1]
@@ -112,62 +131,61 @@ def boyermoore(word):
                 except KeyError:
                     i += length + 1
             else:
+                # If pattern's last letter's comparison is in bad symbol table,
+                # Increases index by it. Otherwise, increases it by pattern's length
                 try:
-                    i += ((btable[line[i]]) + c)
+                    i += btable[line[i]]
                 except KeyError:
                     i += (length + 1)
             a = length
 
 
-def bruteSearch(sentence,word): 
+def brutesearch():
+    global word
     global comparisons
-    sentenceLength = len(sentence)
-    wordLength = len(word)
+    global length
+    global line
+    sentenceLength = len(line)
+    wordLength = length + 1
     for i in range(sentenceLength):
         j = 0
-        while j < wordLength and sentence[i + j] == word[j]:
+        while j < wordLength and line[i + j] == word[j]:
             j = j + 1
-        comparisons = comparisons + j + 1 #This increments count with the number of comparisons
+        comparisons = comparisons + j + 1  #This increments count with the number of comparisons
         #The +1 is because we count the failed comparisons aswell
-        if (j == wordLength):
-            return i
-    return -1
+        if j == wordLength:
+            line = line[:i] + '<mark>' + word + '</mark>' + line[i + len(word):]
+            i += (wordLength + 13)
 
 
-
-
+#.strip() for if input has space in begining or ending
 word = input("Enter the word: ").strip()
 
-#Constructs tables. Since brute force does not need it, we have to change for loop below
-#Length which assigned in bad table, is word's length - 1.
-conbadtable(word)
-congoodtable(word)
 
 method = input("Enter how the string should be found: ")
-if (((method != "brute") and (method != "horspool") and (method != "Boyer"))):
-    print("false input")
+
+
+if method == "brute":
+    for i in range(len(lines)):
+        line = lines[i]
+        brutesearch()
+        lines[i] = line
+elif method == "horspool":
+    conbadtable()
+    for i in range(len(lines)):
+        line = lines[i]
+        horspoolsearch()
+        lines[i] = line
+elif method == "Boyer":
+    conbadtable()
+    congoodtable()
+    for i in range(len(lines)):
+        line = lines[i]
+        boyermoore()
+        lines[i] = line
+else:
+    print("Unknown algorithm")
     sys.exit(1)
-
-
-for i in range(len(lines)):
-    line = lines[i]
-
-    # For optimization all search functions must know length of word.
-    # Also method does not change in loop
-    if (method == "brute"):
-        index = bruteSearch(line,word)
-        if (index != -1):
-            line = line[:index] + '<mark>' + word + '</mark>' + line[index+len(word):]
-            found = True            
-    elif (method == "horspool"):
-        horspoolsearch(word)
-    elif method == "Boyer":
-        boyermoore(word)
-    else:
-        print("horsealgo")
-
-
-    lines[i] = line
 
 print("Number of comparisons: " + str(comparisons))
 
@@ -176,11 +194,3 @@ with open('test.html', 'w') as file:
     
 
 print(time.time() - start_time, "seconds")
-
-
-
-
-
-
-
-
